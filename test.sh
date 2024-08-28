265d496692e26570f33fb90d1b0372329d6de29a
reaper256 <115444718+reaper256@users.noreply.github.com>
2024-08-28 18:15:26 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,3 +2,29 @@
 
 echo "below output is from environment variable"
 echo $GITHUB_WORKSPACE
+
+
+
+#variables to pass to script
+#$1 = variablesFile = absolute path to variables file
+#This script expects 4 folders to be present in ${GITHUB_WORKSPACE} like:
+#1. Artifact-JAR - Contains all the jar files generated from maven build
+#2. Artifact-WorkflowFiles - Contains all rendered workflow files to be deployed
+#3. TBD
+#4. TBD
+
+function abort_on_failure() {
+    echo $1
+    exit 1
+}
+
+function prepareVariableFile () {
+    variablesFile=$1
+    pathToVariablesFile=$(dirname "$variablesFile")
+    cd $pathToVariablesFile
+    jq --arg accountUsername "$accountUsername" --arg accountPwd "$accountPwd" '.account_username = $accountUsername | .account_password = $accountPwd' < $variablesFile > temp.json
+    cp -f temp.json $variablesFile
+}
+
+prepareVariableFile $GITHUB_WORKSPACE/properties.json
+cat $GITHUB_WORKSPACE/properties.json
